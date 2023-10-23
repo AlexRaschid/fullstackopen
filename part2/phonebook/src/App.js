55824d15fa6a13ef917aa9c54edc018603c3f660
AlexRaschid <AlexRaschid@users.noreply.github.com>
2023-10-23 13:46:37 -0400

    2.6 finished
    TODO: 2.7
    TimeSpent: ~1.5hr (including previous commit)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,34 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addNote = (event) => {
+    event.preventDefault();
+
+    //How I would use a "person's name as a value of the key property"
+    //let nameArr = newName.split(' ');
+    //let firstName = newName.split(' ')[0];
+    //let lastName = newName.split(' ')[1];
+
+    const nameObject = {
+      name: newName,
+      //firstName: lastName
+    }
+  
+    setPersons(persons.concat(nameObject));
+    setNewName('');
+  }
+
+  const handleNameInput = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNote}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameInput}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -19,6 +41,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       ...
+
+      <div>debug: {newName}</div>
     </div>
   )
 }
