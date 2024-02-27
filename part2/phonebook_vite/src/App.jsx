047d7e319f0e7b06d0f1502f8a01297ca33cb829
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-02-27 10:52:43 -0500

    2.6 complete
    will add names to the updated persons state, and map them onto the page
    
    filtering names not present, to be in upcoming exercises

--- part2/phonebook_vite/src/App.jsx
+++ part2/phonebook_vite/src/App.jsx
@@ -1,14 +1,61 @@
-import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState
+  ([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+  
+
+  //Updates newName state live as user types
+  const handleTypingName = (input) => 
+  {
+    setNewName(input.target.value);
+  }
+
+  //Adds newName to persons state
+  const handleSubmitName = (name) =>
+  {
+    name.preventDefault(); // prevent the default action of submitting HTML forms
+
+    let nameArr = newName.split(' ');
+    let firstName = nameArr[0];
+    let lastName = nameArr[1];
+
+
+    const nameObject = {
+      name: newName,
+      firstName: lastName
+    }
+
+    setPersons(persons.concat(nameObject));
+    setNewName('');
+    
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmitName}>
+        <div>
+          name: <input value={newName} onChange={handleTypingName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+      {/*Displays all names in persons state
+        TODO: map persons into a component
+      */}
+      {persons.map(person => 
+      <div key={person.name}>{person.name}</div>
+      )}
+
+      <div>debug: {newName}</div>
     </div>
   )
 }
