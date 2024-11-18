5013e427aab9b5aab77c1eae591c51918df9c9de
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-18 16:00:08 -0500

    2.11: complete
    -added persons.json to root
    -used added json-server and axios to open a local  http://localhost:3001/persons in browser
    -data fetches from server using axios with useEffect

--- part2/phonebook_vite/src/App.jsx
+++ part2/phonebook_vite/src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Person from './components/Person';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState
+  /*const [persons, setPersons] = useState
   ([
     { name: 'Arto Hellas', phone: '040-123456', id: 1 },
     { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ]) 
+  ]) */
+
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [newNameFilter, setNewNameFilter] = useState('')
 
-  //Filter, PersonForm, Persons
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
 
   return (
     //2.10 TODO: component for the form for adding new people to the phonebook
