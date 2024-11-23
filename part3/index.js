9f297b7ac63fbd89011d24b49d7958550537b953
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-11-23 17:23:47 -0500

    3.1 complete
    part 3 init

---
+++ part3/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+ app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>');
+ });
+
+ app.get('/api/persons', (req, res) => {
+    res.json(phonebook)
+ })
+
+ app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const note = phonebook.find(note => note.id === id)
+    response.json(note)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    phonebook = phonebook.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+
+  const generateId = () => {
+    const maxId = phonebook.length > 0
+      ? Math.max(...phonebook.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: Boolean(body.important) || false,
+      id: generateId(),
+    }
+  
+    phonebook = phonebook.concat(note)
+  
+    response.json(note)
+  })
+
+ const PORT = 3001
+ app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)   
+ })
